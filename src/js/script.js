aeba81f1b7e7effb10f2948402ba3f00427e4894
hlnmargary <margary.helene@hetic.net>
2019-03-26 15:47:43 +0100

    svg symbols added waouh what was haaaard

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+var glassesList = document.querySelectorAll('.glassesList');
+var hairstylesList = document.querySelectorAll('.hairstylesList');
+var beardsList = document.querySelectorAll('.beardsList');
+var hatsList = document.querySelectorAll('.hatsList');
+
+
